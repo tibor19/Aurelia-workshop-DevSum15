ef4259b9bb28cbfe4e64347f09cfbc9a415fd17a
Tibi Covaci <tibi@covaci.se>
2015-05-27 14:19:58 +0200

    Weekly menu finished

---
+++ src/app.js
@@ -0,0 +1,15 @@
+﻿export class App {
+    configureRouter(config, router){
+        config.title = "Spaghtti code";
+        config.map([
+            {route : ['', 'home'], moduleId : './home', 
+            nav: true, title: 'Home'},
+            {name : 'recipe', route : 'recipe/:id', moduleId : './recipe', 
+            nav : false, title : 'Recipe details'},
+            {route : 'weekly-menu', moduleId : './weekly-menu', 
+            nav : true, title:'Weekly menu'}
+        ]);
+
+        this.router = router;
+    }
+} 
---
+++ src/weekly-menu.js
@@ -0,0 +1,39 @@
+﻿import {HttpClient} from 'aurelia-http-client';
+import {inject} from 'aurelia-framework';
+// import {ImagePathValueConverter} from './converters';
+
+@inject(HttpClient)
+export class WeeklyMenu {
+
+    constructor(http){
+        this.http = http;
+        this.recipes = null;
+        this.url = 'data/recipies.json';
+    }
+
+    activate(){
+        let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+        this.http.get(this.url)
+            .then(response => 
+                this.recipes = response.content.map((recipe, index) => {
+                    recipe.weekday = weekdays[index % weekdays.length];
+                    return recipe;
+                    })
+                );
+    }
+}
+
+export class ImagePathValueConverter {
+
+    toView(value){
+        return value && 'content/images/food/' + value + '.jpg';
+    }
+}
+
+export class WeekDayValueConverter {
+
+    toView(value){
+        let weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+        return weekdays[value % weekdays.length];
+    }
+}
