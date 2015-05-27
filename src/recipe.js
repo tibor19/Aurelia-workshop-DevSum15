08df8bdd7c266bd12bea11aab122e09c2767fcb9
Tibi Covaci <tibi@covaci.se>
2015-05-27 14:26:36 +0200

    Weekly menu finished

---
+++ src/recipe.js
@@ -0,0 +1,16 @@
+﻿import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
+
+@inject(HttpClient)
+export class Recipe{
+
+    constructor(http){
+        this.http = http;
+        this.recipe = null;
+        this.url = 'data/recipe.json';
+    }
+
+    activate(){
+
+    }
+}
